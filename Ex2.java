9431629b770188cac8395ebce34b8355fa362796
John Meyer <john.meyer@app-project.com>
2022-01-26 17:05:47 -0800

    EFJ-197 Put in content for Ex2

--- Ex2.java
+++ Ex2.java
@@ -4,11 +4,11 @@
  * Java datatypes:  invalid  literal.
  */
 
-public class Ex1  {
+public class Ex2  {
 
     public static void main(String[] args) {
         Integer a = Integer.valueOf(7);
-        int c = a * 5.0;
+        int c = a * 5;
         System.out.println(c);
     }
 }
